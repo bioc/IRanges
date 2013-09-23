41149a241fdf1e877243945cb7c676465ab6d4bb
Valerie Obenchain <vobencha@fhcrc.org>
2013-09-23 17:08:48 +0000

    - Fix bug in findOverlaps(); query with zero-width range no longer registers as   a hit. - Update Rle unit tests to handle NA, NaN on multiple platforms.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_12/madman/Rpacks/IRanges@80662 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findOverlaps-methods.R
+++ R/findOverlaps-methods.R
@@ -34,8 +34,8 @@ setMethod("findOverlaps", c("Ranges", "IntervalTree"),
             query <- as(query, "IRanges")
             query_ord <- NULL
             origQuery <- query
-            adjust <- maxgap - minoverlap + 1L
-            if (adjust > 0L)
+            adjust <- (maxgap - minoverlap + 1L) * (width(query) > 0L)
+            if (sum(adjust) > 0L)
               query <-
                 resize(query, width(query) + 2L * adjust, fix = "center")
             unsortedQuery <- query
